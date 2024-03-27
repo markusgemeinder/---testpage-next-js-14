937a467b9423173c0dbd206779a412a127cbe877
markusgemeinder <190774@gmx.de>
2024-03-27 12:44:53 +0100

    Next.js 14 Tutorial 10 Not Found Page (standard and customized 404)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <h2>Page not found</h2>
+      <p>Could not find requested resource</p>
+    </>
+  );
+}
+
+// example of 404 customization implemented inside /products/[productId]/reviews/[reviewId]/page.tsx and not-found.tsx
