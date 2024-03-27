d38faa72c3d047efab06278abe3fd93cf20c1fb2
markusgemeinder <190774@gmx.de>
2024-03-27 13:07:00 +0100

    Next.js 14 Tutorial 12 Private Folders (not visible in browser)

---
+++ src/app/%5Flib-non-private/page.tsx
@@ -0,0 +1,3 @@
+export default function NonPrivateRoute() {
+  return <h1>If you prefix %5F to a folder name you CAN VIEW it in the browser</h1>;
+}
---
+++ src/app/_lib-private/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateRoute() {
+  return <h1>If you prefix _ to a folder name you CANNOT VIEW it in the browser</h1>;
+}
