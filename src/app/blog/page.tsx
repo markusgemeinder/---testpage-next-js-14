ed69a940680a91850e37f2087ad3a261ac2f12d2
markusgemeinder <190774@gmx.de>
2024-03-29 18:34:15 +0100

    Next.js 14 Tutorial 18 Title Metadata (example layout.tsx vs. /blog/page.tsx)

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from 'next';
+
+// version A (compare to layout.tsx >>> result = "Blog | Codevolution")
+export const metadata: Metadata = {
+  title: 'Blog',
+};
+
+// version B (compare to layout.tsx >>> result = "Blog")
+// export const metadata: Metadata = {
+//   title: {
+//     absolute: 'Blog',
+//   },
+// };
+
 export default function Blog() {
   return <h1>My blog</h1>;
 }
