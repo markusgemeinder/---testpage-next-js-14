befb5bd44896a6512266f55a24ca63ff70bc7ac9
markusgemeinder <190774@gmx.de>
2024-04-01 19:53:54 +0200

    Next.js 14 Tutorial 29 Unmatched Routes

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,11 @@
 import Card from '@/components/Card';
+import Link from 'next/link';
 
 export default function Notifications() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href='/complex-dashboard/archived'>Archived</Link>
+    </Card>
+  );
 }
